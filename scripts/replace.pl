899eec4a11efb60fcd81b80e06ccf51c325f2b2d
WangChunye <wcy123@gmail.com>
2019-01-01 12:10:35 +0800

    replace files to workspace

--- scripts/replace.pl
+++ scripts/replace.pl
@@ -6,13 +6,53 @@ use File::stat;
 use DateTime;
 use File::Slurp;
 use POSIX 'strftime';
-my @args = @ARGV;
+use warnings FATAL => 'all';
+use File::Spec;
+use File::Find;
+use File::Basename;
 
-for my $file (@args) {
-    my $c = read_file($file);
-    $c =~ s:```include\nquote (.*) (.*):```{.$1 include=$2}:g;
-#    $c =~ s/title:\s+"(.*)"/title: $1/g;
-    $c =~ s:blog/src/:cpp_src/:g;
-    write_file($file, $c);
+sub all_files {
+    my @ret= ();
+    find({
+        wanted => sub {
+            if(/\.md$/) {
+            push @ret, $File::Find::name;
+            }
+        }
+         }, "blog");
+    return @ret;
 }
-#print dump($info);
+
+
+my @ret = all_files();
+
+for my $f (@ret) {
+    # next if not $f =~ /.*2017-06-18.*/;
+    my $dir = dirname($f);
+    my $file = basename($f);
+    my $workspace = $dir . "/" . substr($file, 0, 10);
+    if ( not  -d $workspace ) {
+        system("mkdir -p $workspace");
+    }
+    my $tmpfile = "/tmp/a.txt";
+    rename($f , $tmpfile);
+    open my $fh1 , $tmpfile or die "Could not open $tmpfile: $!";
+    open my $fh2 , ">", $f  or die "Could not open $f: $!";
+    while (<$fh1>) {
+        if( /```{\.(.*) include=(.*)}/) {
+            my $type = $1;
+            my $include = $2;
+            my $newname =  $workspace . "/" . basename($include);
+            if ( not $newname eq $include ) {
+                rename($include, $newname) or die "Could not rename $include to $newname";
+                print $fh2 "```{.$type include=$newname}\n";
+            }else {
+                print $fh2 $_;
+            }
+        }else {
+            print $fh2 $_;
+        }
+    }
+    close($fh1);
+    close($fh2);
+}
